82e65aa8e199bc80d50e8f530eceb865cd0fd67e
Aditi Ramakrishnan <aditirkrishna@gmail.com>
2025-08-31 11:31:02 +0530

    packaginggg

--- perception-layer/src/__init__.py
+++ perception-layer/src/__init__.py
@@ -18,17 +18,123 @@ __email__ = "perception@evtol-defense.com"
 # Import main components for easy access
 from .utils.config import Config
 from .utils.logging import setup_logging
-from .preprocessing.data_loader import DataLoader
-from .fusion.map_fusion import MapFusion
-from .serving.api import PerceptionAPI
+
+# Import processing components
+from .processing import (
+    # Data ingestion
+    load_raster_data,
+    load_vector_data,
+    load_netcdf_data,
+    load_point_cloud_data,
+    batch_load_data,
+    get_data_info,
+    # Data transformation
+    reproject_raster,
+    reproject_vector,
+    resample_raster,
+    clip_to_bounds,
+    normalize_data,
+    apply_transformation_pipeline,
+    # Data fusion
+    fuse_raster_layers,
+    fuse_vector_layers,
+    combine_datasets,
+    create_unified_dataset,
+    # Quality control
+    perform_quality_checks,
+    validate_processing_results,
+    generate_quality_report,
+    # Output generation
+    generate_planner_outputs,
+    create_tiled_outputs,
+    export_to_formats
+)
+
+# Import geometry components
+from .geometry import (
+    # Terrain analysis
+    compute_slope,
+    compute_aspect,
+    compute_curvature,
+    compute_roughness,
+    compute_terrain_features,
+    TerrainAnalysisError,
+    # Clearance analysis
+    compute_clearance,
+    compute_obstacle_height,
+    compute_landing_zones,
+    compute_corridor_clearance,
+    ClearanceAnalysisError,
+    # Obstacle detection
+    detect_obstacles,
+    classify_obstacles,
+    compute_obstacle_mask,
+    filter_obstacles_by_height,
+    ObstacleDetectionError,
+    # Landing analysis
+    analyze_landing_feasibility,
+    compute_landing_scores,
+    find_optimal_landing_sites,
+    validate_landing_zones,
+    LandingAnalysisError
+)
 
 # Main classes for users
 __all__ = [
     "Config",
-    "DataLoader", 
-    "MapFusion",
-    "PerceptionAPI",
-    "setup_logging"
+    "setup_logging",
+    # Data ingestion
+    "load_raster_data",
+    "load_vector_data", 
+    "load_netcdf_data",
+    "load_point_cloud_data",
+    "batch_load_data",
+    "get_data_info",
+    # Data transformation
+    "reproject_raster",
+    "reproject_vector",
+    "resample_raster",
+    "clip_to_bounds",
+    "normalize_data",
+    "apply_transformation_pipeline",
+    # Data fusion
+    "fuse_raster_layers",
+    "fuse_vector_layers",
+    "combine_datasets",
+    "create_unified_dataset",
+    # Quality control
+    "perform_quality_checks",
+    "validate_processing_results",
+    "generate_quality_report",
+    # Output generation
+    "generate_planner_outputs",
+    "create_tiled_outputs",
+    "export_to_formats",
+    # Terrain analysis
+    "compute_slope",
+    "compute_aspect",
+    "compute_curvature",
+    "compute_roughness",
+    "compute_terrain_features",
+    "TerrainAnalysisError",
+    # Clearance analysis
+    "compute_clearance",
+    "compute_obstacle_height",
+    "compute_landing_zones",
+    "compute_corridor_clearance",
+    "ClearanceAnalysisError",
+    # Obstacle detection
+    "detect_obstacles",
+    "classify_obstacles",
+    "compute_obstacle_mask",
+    "filter_obstacles_by_height",
+    "ObstacleDetectionError",
+    # Landing analysis
+    "analyze_landing_feasibility",
+    "compute_landing_scores",
+    "find_optimal_landing_sites",
+    "validate_landing_zones",
+    "LandingAnalysisError"
 ]
 
 # Configuration management
